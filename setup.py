aeb210bb21d16b77bdd6aef92f4def616cc87cb5
Diego Rodriguez Escalona <diegorodriguezescalona@Diegos-MacBook-Air-2.local>
2024-10-16 15:32:15 -0400

    setup mistake

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # make sure the etl script outputs properly
-p(
+setup(
     name="ETLpipelineder41",
     version="0.1.0",
     description="ETLpipline",
